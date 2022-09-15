6efd8b282bb7e5b2384d5edf8a876a56ad5cecd7
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2022-09-14 22:45:27 -0300

    Strings

---
+++ Prototype.js
@@ -0,0 +1,46 @@
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
+
+Pessoa.prototype.mostrarNome = function () {  // prototipos só exitem em funções
+  return `Seu nome completo: ${this.nome} ${this.sobrenome}`;
+}
+
+
+function Pessoa(nome, sobrenome, idade) { // Boa prática deixar propriedades na função construtora e funcões no prototipo
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+  this.idade = idade;
+}
+
+const p1 = new Pessoa("wallace", "bezerra", 20);
+
+
+// Liste os métodos acessados por 
+// dados criados com NodeList,
+// HTMLCollection, Document
+
+const dom = document.querySelectorAll("li");
+dom.forEach
+dom.values
+
+const dom2 = document.getElementsByTagName("li");
+dom2.item
+dom2.namedItem
+
+// Liste os construtores dos dados abaixo
+const li = document.querySelector('li');
+const ul = document.querySelector('ul');
+
+li; // html li element
+li.click; // function
+li.innerText; // string
+li.value; // number
+li.hidden; // boolean
+li.offsetLeft; // number
+li.click(); //nao tem
+
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name;
+// string
--- script.js
+++ script.js
@@ -1,47 +1,74 @@
-// Crie uma função construtora de Pessoas
-// Deve conter nome, sobrenome e idade
-// Crie um método no protótipo que retorne
-// o nome completo da pessoa
+// Utilizando o foreach na array abaixo,
+// some os valores de Taxa e os valores de Recebimento
 
-Pessoa.prototype.mostrarNome = function () {
-  return `Seu nome completo: ${this.nome} ${this.sobrenome}`;
-}
+const transacoes = [
+  {
+    descricao: 'Taxa do Pão',
+    valor: 'R$ 39',
+  },
+  {
+    descricao: 'Taxa do Mercado',
+    valor: 'R$ 129',
+  },
+  {
+    descricao: 'Recebimento de Cliente',
+    valor: 'R$ 99',
+  },
+  {
+    descricao: 'Taxa do Banco',
+    valor: 'R$ 129',
+  },
+  {
+    descricao: 'Recebimento de Cliente',
+    valor: 'R$ 49',
+  },
+];
+
+let valorTaxa = 0;
+let valorRecebimento = 0;
+
+transacoes.forEach((item) => {
+
+  if (item.descricao.includes("Taxa")) {
+    valorTaxa += parseInt(item.valor.slice(3));
+  }
+  else if (item.descricao.includes("Recebimento")) {
+    valorRecebimento += parseInt(item.valor.slice(3));
+  }
+})
+console.log(`Valor taxa: ${valorTaxa}`);
+console.log(`Valor recebimento: ${valorRecebimento}`);
 
 
-function Pessoa(nome, sobrenome, idade) {
-  this.nome = nome;
-  this.sobrenome = sobrenome;
-  this.idade = idade;
-}
 
-const p1 = new Pessoa("wallace", "bezerra", 20);
+// Retorne uma array com a lista abaixo
+const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
 
+console.log(transportes.split(";"));
 
-// Liste os métodos acessados por 
-// dados criados com NodeList,
-// HTMLCollection, Document
+// Substitua todos os span's por a's
+const html = `<ul>
+                  <li><span>Sobre</span></li>
+                  <li><span>Produtos</span></li>
+                  <li><span>Contato</span></li>
+                </ul>`;
 
-const dom = document.querySelectorAll("li");
-dom.forEach
-dom.values
-
-const dom2 = document.getElementsByTagName("li");
-dom2.item
-dom2.namedItem
+const resultado = html.replace(/[span]+/g, "a");
+console.log(resultado);
 
 
-// Liste os construtores dos dados abaixo
-const li = document.querySelector('li');
-const ul = document.querySelector('ul');
+// Retorne o último caracter da frase
+const frase = 'Melhor do ano!';
+// console.log(frase.length);
+console.log(frase.charAt(frase.length - 1));
 
-li; // html li element
-li.click; // function
-li.innerText; // string
-li.value; // number
-li.hidden; // boolean
-li.offsetLeft; // number
-li.click(); //nao tem
 
-// Qual o construtor do dado abaixo:
-li.hidden.constructor.name;
-// string
+// Retorne o total de taxas
+const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
+let valorTotal = 0;
+transacoes2.forEach((item) => {
+  if (item.toLowerCase().includes("taxa")) {
+    valorTotal += 1;
+  }
+})
+console.log(valorTotal);
